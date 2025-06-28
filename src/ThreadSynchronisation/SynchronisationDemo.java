6adb586f0364e373cb8d84801f8412a4f5e54a77
AKDev32 <amankumar005562@gmail.com>
2025-06-28 19:53:49 +0530

    commit config

--- src/ThreadSynchronisation/SynchronisationDemo.java
+++ src/ThreadSynchronisation/SynchronisationDemo.java
@@ -2,19 +2,20 @@ package ThreadSynchronisation;
 
 public class SynchronisationDemo {
 
-    private static int counter = 0;
+    private static int counter1 = 0;
+    private static int counter2 = 0;
 
     public static void main(String[] args) {
 
         Thread one = new Thread(() -> {
             for (int i = 0; i < 10000; i++) {
-                increment();
+                increment1();
             }
         });
 
         Thread two = new Thread(() -> {
             for (int i = 0; i < 10000; i++) {
-                increment();
+                increment2();
             }
         });
 
@@ -28,11 +29,15 @@ public class SynchronisationDemo {
             throw new RuntimeException(e);
         }
 
-        System.out.println("Counter value : " + counter);
+        System.out.println("Counter value : " + counter1 + " -- " + counter2);
     }
 
-    private synchronized static void increment () {
-        counter++;
+    private synchronized static void increment1 () {
+        counter1++;
+    }
+
+    private synchronized static void increment2 () {
+        counter2++;
     }
 }
 
